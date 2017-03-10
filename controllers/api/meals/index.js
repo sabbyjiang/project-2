2182484beb0a6bdefa9e075a92255acc2bf6b382
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-03-10 09:27:06 -0500

    api meals index router

--- controllers/api/meals/index.js
+++ controllers/api/meals/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+router.get('/all', controller.findAll);
+router.post('/by-meal', controller.findByMeal);
+router.post('/by-dish', controller.findByDish);
+router.post('/new', controller.create);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
--- controllers/planning/meals/index.js
+++ controllers/planning/meals/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.get('/', controller.findAll);
+router.get('/all', controller.findAll);
 router.post('/dish', controller.findByDish);
-router.post('/bymeal', controller.findByMeal);
+router.post('/by-meal', controller.findByMeal);
 
 module.exports = router;
