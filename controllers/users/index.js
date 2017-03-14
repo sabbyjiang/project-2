84e4cc597b77b7d7a4376784f9d7ab2fbf6406e1
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-03-14 13:33:04 -0400

    users controller

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -4,6 +4,9 @@ const passport = require('passport');
 
 const AuthService = require('../../services/auth');
 
+// All routes here are ~/users/
+
+// Posting to base route for creating a new user
 router.post('/', 
     passport.authenticate(
         'local-signup',
@@ -14,15 +17,18 @@ router.post('/',
     )
 );
 
+// Renders the html file for login form 
 router.get('/new', (req, res) => {
     res.render('users/new');
 });
 
+// Logout route
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 
+// Post to login to check credentials
 router.post('/login', passport.authenticate(
     'local-login', 
     {
