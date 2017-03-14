cf23be23d4934912980899aea7e0acc795f28276
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-03-14 13:34:50 -0400

    planning router

--- controllers/planning/index.js
+++ controllers/planning/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
+// all routes here are based in ~/planning
+
+// uses the user router
 router.use('/user', require('./user'));
+// to search router
 router.use('/search', require('./search'));
+// Redirects any attempt on just ~/planning to the user page
 router.get('/', (req, res) => {
     res.redirect('/planning/user');
 })
