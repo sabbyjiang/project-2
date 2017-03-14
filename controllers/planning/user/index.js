07d482650b9539a588979b31973c9e394a263e3e
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-03-14 13:37:36 -0400

    user router

--- controllers/planning/user/index.js
+++ controllers/planning/user/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const recipes = require('../../../models/recipes-model');
 
+// Routing here: ~/planning/user
+
+// Routes to /meals
 router.use('/meals', require('./meals'));
+
+// Routes to /recipes
 router.use('/recipes', require('./recipes'));
+
+// Finds the most recent recipes from the user and renders it in the index page
 router.get('/', (req, res) => {
     recipes.findSome(req.user.id)
         .then(data => {
